a09bf122e4ee25d377da2e1aa4785ec7a2bc531d
lim <lim1202@gmail.com>
2022-03-09 15:08:28 +0800

    Study plan algorithms day 3

---
+++ solutions/283.moveZeroes.py
@@ -0,0 +1,48 @@
+from typing import List
+
+"""
+283. Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/move-zeroes
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        left = right = 0
+        while right < n:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+            
+if __name__ == "__main__":
+    nums = [0,1,0,3,12]
+    print("Input: nums =", nums)
+    Solution().moveZeroes(nums)
+    print("Output: nums =", nums)
