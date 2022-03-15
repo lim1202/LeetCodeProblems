25553a038d04d0203737917a95cd999fb74e1dec
lim <lim1202@gmail.com>
2022-03-15 11:32:24 +0800

    Study plan algorithms day 7

---
+++ solutions/695.maxAreaOfIsland.py
@@ -0,0 +1,73 @@
+"""
+695. Max Area of Island
+
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/max-area-of-island
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ans = 0
+
+        for i, l in enumerate(grid):
+            for j, v in enumerate(l):
+                if grid[i][j] == 0:
+                    continue
+                ans = max(ans, self.backtrack(grid, i, j))
+
+        return ans
+
+    def backtrack(self, grid: List[List[int]], i: int, j: int) -> int:
+            grid[i][j] = 0
+            depth = 1
+            for di, dj in [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]:
+                if di < 0 or di >= len(grid) or dj < 0 or dj >= len(grid[0]):
+                    continue
+                if grid[di][dj] == 0:
+                    continue
+                depth += self.backtrack(grid, di, dj)   
+            return depth
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    ]
+    print("Input: grid =", grid)
+    print("Output:", Solution().maxAreaOfIsland(grid))
---
+++ solutions/733.floodFill.py
@@ -0,0 +1,75 @@
+"""
+733. Flood Fill
+
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
+
+Return the modified image after performing the flood fill.
+
+Example 1:
+
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
+Output: [[2,2,2],[2,2,2]]
+
+Constraints:
+
+m == image.length
+n == image[i].length
+1 <= m, n <= 50
+0 <= image[i][j], newColor < 216
+0 <= sr < m
+0 <= sc < n
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/flood-fill
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+import collections
+from typing import List
+
+
+class Solution:
+    def floodFill(
+        self, image: List[List[int]], sr: int, sc: int, newColor: int
+    ) -> List[List[int]]:
+        curColor = image[sr][sc]
+        if curColor == newColor:
+            return image
+
+        n, m = len(image), len(image[0])
+        que = collections.deque([(sr, sc)])
+        image[sr][sc] = newColor
+
+        while que:
+            (x, y) = que.popleft()
+            for mx, my in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                if 0 <= mx < n and 0 <= my < m and image[mx][my] == curColor:
+                    que.append((mx, my))
+                    image[mx][my] = newColor
+
+        return image
+
+
+if __name__ == "__main__":
+    image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+    sr = 1
+    sc = 1
+    newColor = 2
+    print(
+        "Input: image = {}, sr = {}, sc = {}, newColor = {}".format(
+            image, sr, sc, newColor
+        )
+    )
+    print("Output:", Solution().floodFill(image, sr, sc, newColor))
